f4fa4645ba264231492977068eb61ccd31491802
inql <dbinkus96@gmail.com>
2019-11-24 18:08:10 +0100

    Add init values for GildedRoseTest unit tests

--- src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -2,11 +2,19 @@ package com.gildedrose
 
 import org.scalatest._
 
-class GildedRoseTest  extends FlatSpec with Matchers {
-      it should "foo" in {
-        val items = Array[Item](new Item("foo", 0, 0))
-        val app = new GildedRose(items)
-        app.updateQuality()
-        (app.items(0).name) should equal ("foo")
-      }
+class GildedRoseTest extends FunSpec with BeforeAndAfterEach {
+
+  var regular, agedBrie, sulfuras, backstage: Item = Nil
+  var gildedRose: GildedRose = _
+
+  override def beforeEach(): Unit = {
+    regular = new Item("regular", 7, 7)
+    agedBrie = new Item("Aged Brie", 15, 15)
+    sulfuras = new Item("Sulfuras, Hand of Ragnaros", 30,80)
+    backstage = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 8)
+
+    gildedRose = new GildedRose(Array(regular,agedBrie,sulfuras,backstage))
+  }
+
+
 }
