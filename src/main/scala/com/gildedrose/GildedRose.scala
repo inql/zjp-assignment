a072597fc617f388b6eafc786d52b55c8701ecbb
inql <dbinkus96@gmail.com>
2019-11-25 23:47:56 +0100

    Add refactored version of updateQuality function (using pattern matching)

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -2,63 +2,40 @@ package com.gildedrose
 
 class GildedRose(val items: Array[Item]) {
 
-
-  def updateQuality() {
-    for (i <- 0 until items.length) {
-      if (!items(i).name.equals("Aged Brie")
-        && !items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (items(i).quality > 0) {
-          if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-            items(i).quality = items(i).quality - 1
-          }
-        }
-      } else {
-        if (items(i).quality < 50) {
-          items(i).quality = items(i).quality + 1
-
-          if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).sellIn < 11) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
-
-            if (items(i).sellIn < 6) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
-          }
-        }
-      }
-
-      if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-        items(i).sellIn = items(i).sellIn - 1
-      }
-
-      if (items(i).sellIn < 0) {
-        if (!items(i).name.equals("Aged Brie")) {
-          if (!items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).quality > 0) {
-              if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-                items(i).quality = items(i).quality - 1
-              }
-            }
-          } else {
-            items(i).quality = items(i).quality - items(i).quality
-          }
-        } else {
-          if (items(i).quality < 50) {
-            items(i).quality = items(i).quality + 1
-          }
-        }
-      }
-    }
-  }
-
   def setMaxQuality(item: Item, updateValue: Int, maxQuality: Int): Item = {
     item.quality += updateValue
     item.quality = item.quality min maxQuality
     item
+  }
+
+  def updateQuality(): Unit = {
+    items.foreach {
+      case Item("Sulfuras, Hand of Ragnaros", _, _) =>
+      case item@Item("Aged Brie", sellIn, quality) if quality <= 50 & sellIn >= 0 =>
+        item.sellIn -= 1
+        setMaxQuality(item, 1, 50)
+      case item@Item("Aged Brie", sellIn, quality) if quality <= 50 & sellIn < 0 =>
+        item.sellIn -= 1
+        setMaxQuality(item, 2, 50)
+      case item@Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality) if quality < 50 && sellIn > 10 =>
+        item.sellIn -= 1
+        setMaxQuality(item, 1, 50)
+      case item@Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality) if quality < 50 && sellIn > 5 =>
+        item.sellIn -= 1
+        setMaxQuality(item, 2, 50)
+      case item@Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality) if quality < 50 && sellIn > 0 =>
+        item.sellIn -= 1
+        setMaxQuality(item, 3, 50)
+      case item@Item("Backstage passes to a TAFKAL80ETC concert", sellIn, _) if sellIn <= 0 =>
+        item.sellIn -= 1
+        item.quality = 0
+      case item@Item(_, sellIn, quality) if sellIn > 0 && quality > 0 =>
+        item.sellIn -= 1
+        item.quality -= 1
+      case item@Item(_, sellIn, quality) if sellIn <= 0 && quality > 0 =>
+        item.sellIn -= 1
+        item.quality -= 2
+      case _ =>
+    }
   }
 }
