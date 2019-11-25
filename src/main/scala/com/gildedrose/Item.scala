f668e3ddc5e40b079de813d564f355d2531316c5
inql <dbinkus96@gmail.com>
2019-11-26 00:52:28 +0100

    Add definition for Item case class

--- src/main/scala/com/gildedrose/Item.scala
+++ src/main/scala/com/gildedrose/Item.scala
@@ -1,5 +1,9 @@
 package com.gildedrose
 
-final case class Item(name: String, var sellIn: Int, var quality: Int) {
-
-}
+/**
+ * A case class which contains information about specific item.
+ * @param name Name of the item.
+ * @param sellIn Defines when the item should be sold.
+ * @param quality Current quality value.
+ */
+final case class Item(name: String, var sellIn: Int, var quality: Int)
