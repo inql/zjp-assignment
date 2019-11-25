f4275477827c4f0ffddce0b8038603f72259f63a
inql <dbinkus96@gmail.com>
2019-11-26 00:30:36 +0100

    Defined a constants to avoid duplicating literals

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -2,6 +2,10 @@ package com.gildedrose
 
 class GildedRose(val items: Array[Item]) {
 
+  val Sulfuras = "Sulfuras, Hand of Ragnaros"
+  val AgedBrie = "Aged Brie"
+  val Backstage = "Backstage passes to a TAFKAL80ETC concert"
+
   def setMaxQuality(item: Item, updateValue: Int, maxQuality: Int): Item = {
     item.quality += updateValue
     item.quality = item.quality min maxQuality
@@ -10,23 +14,23 @@ class GildedRose(val items: Array[Item]) {
 
   def updateQuality(): Unit = {
     items.foreach {
-      case Item("Sulfuras, Hand of Ragnaros", _, _) =>
-      case item@Item("Aged Brie", sellIn, quality) if quality <= 50 & sellIn >= 0 =>
+      case Item(Sulfuras, _, _) =>
+      case item@Item(AgedBrie, sellIn, quality) if quality <= 50 & sellIn >= 0 =>
         item.sellIn -= 1
         setMaxQuality(item, 1, 50)
-      case item@Item("Aged Brie", sellIn, quality) if quality <= 50 & sellIn < 0 =>
+      case item@Item(AgedBrie, sellIn, quality) if quality <= 50 & sellIn < 0 =>
         item.sellIn -= 1
         setMaxQuality(item, 2, 50)
-      case item@Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality) if quality < 50 && sellIn > 10 =>
+      case item@Item(Backstage, sellIn, quality) if quality < 50 && sellIn > 10 =>
         item.sellIn -= 1
         setMaxQuality(item, 1, 50)
-      case item@Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality) if quality < 50 && sellIn > 5 =>
+      case item@Item(Backstage, sellIn, quality) if quality < 50 && sellIn > 5 =>
         item.sellIn -= 1
         setMaxQuality(item, 2, 50)
-      case item@Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality) if quality < 50 && sellIn > 0 =>
+      case item@Item(Backstage, sellIn, quality) if quality < 50 && sellIn > 0 =>
         item.sellIn -= 1
         setMaxQuality(item, 3, 50)
-      case item@Item("Backstage passes to a TAFKAL80ETC concert", sellIn, _) if sellIn <= 0 =>
+      case item@Item(Backstage, sellIn, _) if sellIn <= 0 =>
         item.sellIn -= 1
         item.quality = 0
       case item@Item(_, sellIn, quality) if sellIn > 0 && quality > 0 =>
