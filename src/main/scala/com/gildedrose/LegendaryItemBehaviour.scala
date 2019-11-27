734f5c6a88cd036936624e466d63cf6cd8f17d76
inql <dbinkus96@gmail.com>
2019-11-27 23:26:53 +0100

    Move comments into methods body

--- src/main/scala/com/gildedrose/LegendaryItemBehaviour.scala
+++ src/main/scala/com/gildedrose/LegendaryItemBehaviour.scala
@@ -6,9 +6,12 @@
 package com.gildedrose
 
 class LegendaryItemBehaviour(item: Item) extends Behaviour(item){
-  //Legendary item never changes it's value so this implementation is empty.
-  override def update():Unit = {}
-  
-  //Legendary item never changes it's value so this implementation is empty.
-  override protected def updateQualityValue(): Unit = {}
+
+  override def update():Unit = {
+    //Legendary item never changes it's value so this implementation is empty.
+  }
+
+  override protected def updateQualityValue(): Unit = {
+    //Legendary item never changes it's value so this implementation is empty.
+  }
 }
