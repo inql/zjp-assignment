195c770c23400cc39105f397dd67db4dc534215b
inql <dbinkus96@gmail.com>
2019-11-27 22:20:35 +0100

    Add implementation for DefaultBehaviour

---
+++ src/main/scala/com/gildedrose/DefaultBehaviour.scala
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019 Dawid Bińkuś
+ * All rights reserved.
+ */
+
+package com.gildedrose
+
+class DefaultBehaviour(item: Item) extends Behaviour(item){
+  override protected def updateQualityValue: Unit = {
+    item.quality = if (item.sellIn < 0) item.quality - 2 else item.quality - 1
+  }
+
+  override protected def updateSellInValue: Unit = item.sellIn -= 1
+}
