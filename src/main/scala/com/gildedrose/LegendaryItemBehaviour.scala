9d6330edbff14920a5b58dcf2e516cec111e94c9
inql <dbinkus96@gmail.com>
2019-11-27 23:23:31 +0100

    Replace multiline comments with nested one in LegendaryItemBehaviour

--- src/main/scala/com/gildedrose/LegendaryItemBehaviour.scala
+++ src/main/scala/com/gildedrose/LegendaryItemBehaviour.scala
@@ -6,13 +6,9 @@
 package com.gildedrose
 
 class LegendaryItemBehaviour(item: Item) extends Behaviour(item){
-  /**
-   * Legendary item never changes it's value so this implementation is empty.
-   */
+  //Legendary item never changes it's value so this implementation is empty.
   override def update():Unit = {}
-
-  /**
-   * Legendary item never changes it's value so this implementation is empty.
-   */
+  
+  //Legendary item never changes it's value so this implementation is empty.
   override protected def updateQualityValue(): Unit = {}
 }
